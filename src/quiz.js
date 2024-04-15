ea229f00685df51925edb7cbb528d6453439544e
Marie Rabel <marie.rabel0697@gmail.com>
2024-04-15 17:55:42 +0200

    Co-authored-by: GuillaumeDurame <GuillaumeDurame@users.noreply.github.com>

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,46 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+  // 2. getQuestion()
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+  // 3. moveToNextQuestion()
+  moveToNextQuestion() {
+    return (this.currentQuestionIndex += 1);
+  }
+  // 4. shuffleQuestions()
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.questions[i], this.questions[j]] = [
+        this.questions[j],
+        this.questions[i],
+      ];
+    }
+  }
+  // 5. checkAnswer(answer)
+  checkAnswer(answer) {
+    const currentQuestion = this.questions[this.currentQuestionIndex];
+    if (answer === currentQuestion.answer) {
+      this.correctAnswers++;
+    }
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
-
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+  // 6. hasEnded()
+  hasEnded() {
+    if (this.currentQuestionIndex === Quiz.length) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
